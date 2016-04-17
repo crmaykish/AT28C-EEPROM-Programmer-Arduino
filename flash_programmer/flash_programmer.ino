6fa1bb0e6c13cfde56fa35ce8c8ac6dc75aa8e5e
Colin Maykish <crmaykish@gmail.com>
2016-04-17 17:46:37 -0400

    Switched to Atmel AT28C series EEPROM, PoC write working

--- flash_programmer/flash_programmer.ino
+++ flash_programmer/flash_programmer.ino
@@ -1,10 +1,10 @@
-// AM29F040 Flash Memory Programmer
+// AT28C Flash Memory Programmer
 
-const int CE = 4;
-const int OE = 2;
-const int WE = 3;
-const int DQ[] = {22, 23, 24, 25, 26, 27, 28, 29};
-const int A[] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48};
+const int CE = 2;
+const int OE = 3;
+const int WE = 4;
+const int I[] = {22, 23, 24, 25, 26, 27, 28, 29};
+const int A[] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42};
 
 void setup() {
   pinMode(OE, OUTPUT);
@@ -16,7 +16,7 @@ void setup() {
   digitalWrite(CE, HIGH);
 
   // Set address bus to outputs
-  for (int i = 0; i < 19; i++){
+  for (int i = 0; i < 13; i++){
     pinMode(A[i], OUTPUT);
   }
 
@@ -24,23 +24,18 @@ void setup() {
 
   delay(50);
 
-  storeValue(0, 0);
-  storeValue(15, 0);
+  writeByte(0, 15);
+  writeByte(1, 23);
+  delay(100);
+  
 }
 
 void loop() {
   while (Serial.available() > 0){
     int b = Serial.parseInt();
     readByte(b);
-    delay(5);
+    delay(10);
   }
-}
-
-void storeValue(int addr, int val){
-  writeByte(0x5555, 0xAA);
-  writeByte(0x2AAA, 0x55);
-  writeByte(0x5555, 0xA0);
-  writeByte(addr, val);
 }
 
 void writeByte(int addr, int val){
@@ -48,14 +43,18 @@ void writeByte(int addr, int val){
   Serial.print(val);
   Serial.print(" to ");
   Serial.println(addr);
+
+  // Disable output
+  digitalWrite(OE, HIGH);
+  delay(50);
   
   // Set data to outputs
   for (int i = 0; i < 8; i++){
-    pinMode(DQ[i], INPUT);
+    pinMode(I[i], OUTPUT);
   }
 
   // set address
-  for (int i = 0; i < 19; i++){
+  for (int i = 0; i < 13; i++){
     int a = (addr & (1 << i)) > 0;
     digitalWrite(A[i], a);
   }
@@ -64,37 +63,40 @@ void writeByte(int addr, int val){
   if (val < 256) {
     for (int i = 0; i < 8; i++){
       int a = (val & (1 << i)) > 0;
-      digitalWrite(DQ[i], a);
+      digitalWrite(I[i], a);
     }
   }
 
   digitalWrite(CE, LOW);
+  delay(10);
   digitalWrite(WE, LOW);
   delay(10);
   digitalWrite(WE, HIGH);
+  delay(10);
   digitalWrite(CE, HIGH);
+  delay(10);
 }
 
 void readByte(int addr){
   // Set data pins to inputs
   for (int i = 0; i < 8; i++){
-    pinMode(DQ[i], INPUT);
+    pinMode(I[i], INPUT);
   }
 
   // Write the addr
-  for (int i = 0; i < 19; i++){
+  for (int i = 0; i < 13; i++){
     int a = (addr & (1 << i)) > 0;
     digitalWrite(A[i], a);
   }
 
   digitalWrite(CE, LOW);
   digitalWrite(OE, LOW);
-  delay(1);
+  delay(10);
 
   // Read data bus
   Serial.print("Read val: ");
   for (int i = 0; i < 8; i++){
-    int d = digitalRead(DQ[i]);
+    int d = digitalRead(I[i]);
     Serial.print(d);
     Serial.print(" ");
   }
@@ -102,5 +104,6 @@ void readByte(int addr){
 
   digitalWrite(OE, HIGH);
   digitalWrite(CE, HIGH);
+  delay(10);
 }
 
