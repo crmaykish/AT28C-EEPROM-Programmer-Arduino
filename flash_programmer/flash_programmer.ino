bf83f7effceccac4fc4e3fd2ee87bfb8226031d9
Colin Maykish <crmaykish@gmail.com>
2016-04-17 21:33:47 -0400

    Some cleanup

--- flash_programmer/flash_programmer.ino
+++ flash_programmer/flash_programmer.ino
@@ -1,109 +1,107 @@
 // AT28C Flash Memory Programmer
+// Colin Maykish (cmaykish.com)
+// 04/17/2016
 
+// Width in bits of address bus (64K has 13, 256K has 15)
+const int ADDR_BUS_WIDTH = 13;
+
+// Pins
 const int CE = 2;
 const int OE = 3;
 const int WE = 4;
-const int I[] = {22, 23, 24, 25, 26, 27, 28, 29};
-const int A[] = {30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42};
+const int I[] = { 22, 23, 24, 25, 26, 27, 28, 29 };
+const int A[] = { 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42 };
+
+// Set data bus to INPUT or OUTPUT
+void setDataBusMode(int mode) {
+	if (mode == INPUT || mode == OUTPUT) {
+		for (int i = 0; i < 8; i++) {
+			pinMode(I[i], mode);
+		}
+	}
+}
+
+// Write an address to the address bus
+void setAddress(int addr) {
+	for (int i = 0; i < ADDR_BUS_WIDTH; i++) {
+		int a = (addr & (1 << i)) > 0;
+		digitalWrite(A[i], a);
+	}
+}
 
 void setup() {
-  pinMode(OE, OUTPUT);
-  pinMode(WE, OUTPUT);
-  pinMode(CE, OUTPUT);
+	Serial.begin(9600);
 
-  digitalWrite(OE, HIGH);
-  digitalWrite(WE, HIGH);
-  digitalWrite(CE, HIGH);
+	// Define control pins and turn them all off
+	pinMode(OE, OUTPUT);
+	pinMode(WE, OUTPUT);
+	pinMode(CE, OUTPUT);
+	digitalWrite(OE, HIGH);
+	digitalWrite(WE, HIGH);
+	digitalWrite(CE, HIGH);
 
-  // Set address bus to outputs
-  for (int i = 0; i < 13; i++){
-    pinMode(A[i], OUTPUT);
-  }
+	// Set address bus to outputs
+	for (int i = 0; i < ADDR_BUS_WIDTH; i++) {
+		pinMode(A[i], OUTPUT);
+	}
 
-  Serial.begin(9600);
-
-  delay(50);
-
-  writeByte(0, 15);
-  writeByte(1, 23);
-  delay(100);
-  
+	writeByte(0, 15);
+	writeByte(1, 23);
 }
 
 void loop() {
-  while (Serial.available() > 0){
-    int b = Serial.parseInt();
-    readByte(b);
-    delay(10);
-  }
+	while (Serial.available() > 0) {
+		int b = Serial.parseInt();
+		Serial.println(readByte(b));
+	}
 }
 
-void writeByte(int addr, int val){
-  Serial.print("Write: ");
-  Serial.print(val);
-  Serial.print(" to ");
-  Serial.println(addr);
+void writeByte(int addr, byte val) {
+	digitalWrite(OE, HIGH);
+	setDataBusMode(OUTPUT);
+	setAddress(addr);
 
-  // Disable output
-  digitalWrite(OE, HIGH);
-  delay(50);
-  
-  // Set data to outputs
-  for (int i = 0; i < 8; i++){
-    pinMode(I[i], OUTPUT);
-  }
+	// Send data value to data bus
+	for (int i = 0; i < 8; i++) {
+		int a = (val & (1 << i)) > 0;
+		digitalWrite(I[i], a);
+	}
 
-  // set address
-  for (int i = 0; i < 13; i++){
-    int a = (addr & (1 << i)) > 0;
-    digitalWrite(A[i], a);
-  }
-
-  // set data
-  if (val < 256) {
-    for (int i = 0; i < 8; i++){
-      int a = (val & (1 << i)) > 0;
-      digitalWrite(I[i], a);
-    }
-  }
-
-  digitalWrite(CE, LOW);
-  delay(10);
-  digitalWrite(WE, LOW);
-  delay(10);
-  digitalWrite(WE, HIGH);
-  delay(10);
-  digitalWrite(CE, HIGH);
-  delay(10);
+	// Commit data write
+	digitalWrite(CE, LOW);
+	delay(10);
+	digitalWrite(WE, LOW);
+	delay(10);
+	digitalWrite(WE, HIGH);
+	delay(10);
+	digitalWrite(CE, HIGH);
+	delay(10);
 }
 
-void readByte(int addr){
-  // Set data pins to inputs
-  for (int i = 0; i < 8; i++){
-    pinMode(I[i], INPUT);
-  }
+byte readByte(int addr) {
+	byte data = 0;
 
-  // Write the addr
-  for (int i = 0; i < 13; i++){
-    int a = (addr & (1 << i)) > 0;
-    digitalWrite(A[i], a);
-  }
+	setDataBusMode(INPUT);
 
-  digitalWrite(CE, LOW);
-  digitalWrite(OE, LOW);
-  delay(10);
+	// Write the addr
+	for (int i = 0; i < ADDR_BUS_WIDTH; i++) {
+		int a = (addr & (1 << i)) > 0;
+		digitalWrite(A[i], a);
+	}
 
-  // Read data bus
-  Serial.print("Read val: ");
-  for (int i = 0; i < 8; i++){
-    int d = digitalRead(I[i]);
-    Serial.print(d);
-    Serial.print(" ");
-  }
-  Serial.println("");
+	digitalWrite(CE, LOW);
+	digitalWrite(OE, LOW);
+	delay(10);
 
-  digitalWrite(OE, HIGH);
-  digitalWrite(CE, HIGH);
-  delay(10);
+	// Read data bus
+	for (int i = 0; i < 8; i++) {
+		int d = digitalRead(I[i]);
+		data += (d << i);
+	}
+
+	digitalWrite(OE, HIGH);
+	digitalWrite(CE, HIGH);
+	delay(10);
+
+	return data;
 }
-
